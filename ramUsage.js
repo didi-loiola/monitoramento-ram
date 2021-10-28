0dc16ed4436a98fb7ea7a6038f11e733ab3ce538
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-28 11:59:11 -0300

    Adicionando mais status do sistema e de forma amigável

--- ramUsage.js
+++ ramUsage.js
@@ -2,14 +2,18 @@ const os = require('os');
 
 const { arch, platform, totalmem, freemem } = os;
 
-const tRam = (totalmem()/1024/1024).toFixed(0);
-const fRam = (freemem()/1024/1024).toFixed(0);
+setInterval(()=>{
+    const tRam = (totalmem()/1024/1024).toFixed(0);
+    const fRam = (freemem()/1024/1024).toFixed(0);
+    const usage = ((tRam-fRam)/tRam * 100).toFixed(2);
 
-const status = {
+    const status = {
     OS: platform(),
     Arch: arch(),
-    TotalRam: tRam,
-    FreeRam: fRam
-}
-
-console.log(status)
+    TotalRam: `${tRam} MB`,
+    FreeRam: `${fRam} MB`,
+    Usage: `${usage} %`
+    }
+    console.clear()
+    console.table(status)
+}, 1000)
