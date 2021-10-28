07ab91415a4fd253be93f2b740733b3d3b15df07
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-28 11:47:16 -0300

    Melhorando a visualização da memória ram

--- ramUsage.js
+++ ramUsage.js
@@ -2,11 +2,14 @@ const os = require('os');
 
 const { arch, platform, totalmem, freemem } = os;
 
+const tRam = (totalmem()/1024/1024).toFixed(0);
+const fRam = (freemem()/1024/1024).toFixed(0);
+
 const status = {
     OS: platform(),
     Arch: arch(),
-    TotalRam: totalmem(),
-    FreeRam: freemem()
+    TotalRam: tRam,
+    FreeRam: fRam
 }
 
 console.log(status)
