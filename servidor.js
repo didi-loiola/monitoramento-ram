cb98503d68a2ce19c039cbb5ff50186beea5a8c7
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-29 09:50:59 -0300

    Adicionando rota para monitoramento da memória

--- servidor.js
+++ servidor.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const host = 'http://localhost';
 const port = 4000
+const status = require('./ramUsage.js')
 
 http.createServer((req,res)=>{
-    res.end(`<h1>It's Working</h1>`)
+    let url = req.url;
+
+    if(url === '/status'){
+        res.end(JSON.stringify(status, null,2));
+    } else {
+        res.end(`<h1>Bem vindo!</h1>`)
+    }
 }).listen(port,()=>console.log(`Server is running in ${host}:${port}`))
