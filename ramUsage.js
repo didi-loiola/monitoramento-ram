c075630bd22f26a191e2eb8ef0b2ddc173f36a6c
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-29 09:51:31 -0300

    Adicionando seção uptime em horas

--- ramUsage.js
+++ ramUsage.js
@@ -1,19 +1,22 @@
 const os = require('os');
 
-const { arch, platform, totalmem, freemem } = os;
+const { arch, platform, totalmem, freemem, uptime, cpus } = os;
 
 setInterval(()=>{
     const tRam = (totalmem()/1024/1024).toFixed(0);
     const fRam = (freemem()/1024/1024).toFixed(0);
     const usage = ((tRam-fRam)/tRam * 100).toFixed(2);
+    const uTime = (uptime()/60/60).toFixed(1);
 
     const status = {
-    OS: platform(),
-    Arch: arch(),
-    TotalRam: `${tRam} MB`,
-    FreeRam: `${fRam} MB`,
-    Usage: `${usage} %`
+        OS: platform(),
+        Arch: arch(),
+        Uptime: `${uTime} hs`,
+        TotalRam: `${tRam} MB`,
+        FreeRam: `${fRam} MB`,
+        Usage: `${usage} %`
     }
-    console.clear()
-    console.table(status)
+    console.clear();
+    console.table(status);
+    exports.status = status;
 }, 1000)
